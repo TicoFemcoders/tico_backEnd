f048f52cde0e641a9bab99aeb3c35c98052518ca
Nuclea88 <ingrid.lopov@gmail.com>
2026-04-07 19:02:07 +0200

    chore: first commit with dependencies and dotenv enabled

---
+++ src/test/java/com/femcoders/tico/TicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.tico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
