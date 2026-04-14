8c36a49803f8675f9a46d11f0387c2c6362da135
marie charlotte <charlottedoulcet90@gmail.com>
2026-04-14 17:06:35 +0200

    feat(controller): add ActivationController

---
+++ src/main/java/com/femcoders/tico/controller/ActivationController.java
@@ -0,0 +1,34 @@
+package com.femcoders.tico.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.femcoders.tico.dto.request.ActivationRequestDTO;
+import com.femcoders.tico.dto.request.ResendCodeRequestDTO;
+import com.femcoders.tico.service.ActivationService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("api/activation")
+public class ActivationController {
+
+  @Autowired
+  private ActivationService activationService;
+
+  @PostMapping("/activate")
+  public ResponseEntity<Void> activate(@Valid @RequestBody ActivationRequestDTO dto) {
+    activationService.activateAccount(dto);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/resend")
+  public ResponseEntity<Void> resend(@Valid @RequestBody ResendCodeRequestDTO dto) {
+    activationService.resendCode(dto.email());
+    return ResponseEntity.ok().build();
+  }
+}
