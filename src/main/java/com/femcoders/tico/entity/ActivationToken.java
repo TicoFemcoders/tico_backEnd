238bfe88d82544604a26448e1ad025309dd7f87b
marie charlotte <charlottedoulcet90@gmail.com>
2026-04-14 11:24:19 +0200

    feat(entity): add ActivationToken

---
+++ src/main/java/com/femcoders/tico/entity/ActivationToken.java
@@ -0,0 +1,53 @@
+package com.femcoders.tico.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+
+import com.femcoders.tico.enums.TokenType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "activation_tokens")
+@Data
+@NoArgsConstructor
+public class ActivationToken {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false, length = 6)
+  private String code;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 10)
+  private TokenType type;
+
+  @Column(nullable = false)
+  private LocalDateTime expiresAt;
+
+  @Column(nullable = false)
+  private boolean used = false;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createAt;
+}
