bd926bd7e34977571354dd7ce49be53eb9b5f2d1
marie charlotte <charlottedoulcet90@gmail.com>
2026-04-14 16:59:28 +0200

    feat(user): update UserService, UserServiceImpl, UserController

--- src/main/java/com/femcoders/tico/service/UserServiceImpl.java
+++ src/main/java/com/femcoders/tico/service/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.femcoders.tico.service;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.femcoders.tico.dto.request.UserRegisterReqDTO;
+import com.femcoders.tico.dto.request.AdminCreateUserReqDTO;
 import com.femcoders.tico.dto.response.UserResponseDTO;
 import com.femcoders.tico.entity.User;
+import com.femcoders.tico.enums.TokenType;
 import com.femcoders.tico.mapper.UserMapper;
 import com.femcoders.tico.repository.UserRepository;
 import com.femcoders.tico.security.UserDetail;
@@ -27,23 +30,30 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private ActivationService activationService;
+
+    @Autowired
+    private EmailService emailService;
+
     @Override
     @Transactional
-    public UserResponseDTO createUser(UserRegisterReqDTO userDto) {
-       
+    public UserResponseDTO createUser(AdminCreateUserReqDTO userDto) {
         User user = userMapper.toEntity(userDto);
-
-        user.setPasswordHash(passwordEncoder.encode(userDto.password()));
-        
+        user.setPasswordHash(passwordEncoder.encode(UUID.randomUUID().toString()));
         User savedUser = userRepository.save(user);
-        
+
+        String code = activationService.generateCodeAndSaveToken(savedUser, TokenType.ACTIVATION);
+        emailService.sendActivationCode(savedUser.getEmail(), savedUser.getName(), code);
+        emailService.sendActivationLink(savedUser.getEmail(), savedUser.getName());
+
         return userMapper.toResponseDTO(savedUser);
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-        .map(UserDetail::new)
-        .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+                .map(UserDetail::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
     }
 }
