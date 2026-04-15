45661228fa35941153904fa252b18f7d2b7619ed
marie charlotte <charlottedoulcet90@gmail.com>
2026-04-15 12:09:10 +0200

    fix : Swap TicketsResponseDTO form class to record.

--- src/main/java/com/femcoders/tico/dto/response/TicketsResponseDTO.java
+++ src/main/java/com/femcoders/tico/dto/response/TicketsResponseDTO.java
@@ -6,23 +6,17 @@ import java.util.Set;
 import com.femcoders.tico.enums.TicketPriority;
 import com.femcoders.tico.enums.TicketStatus;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-public class TicketsResponseDTO {
-
-    private Long id;
-    private String title;
-    private String description;
-    private TicketStatus status;
-    private TicketPriority priority;
-    private Long createdById;
-    private Long assignedToId;
-    private Set<String> labels;
-    private String emailSubject;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime closedAt;
+public record TicketsResponseDTO(
+        Long id,
+        String title,
+        String description,
+        TicketStatus status,
+        TicketPriority priority,
+        Long createdById,
+        Long assignedToId,
+        Set<String> labels,
+        String emailSubject,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        LocalDateTime closedAt) {
 }
